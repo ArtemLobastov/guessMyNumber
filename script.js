178a3c988613395c9dfb51c9beecb0c2cb9f5d62
Artem Lobastov <102912371+ArtemLobastov@users.noreply.github.com>
2023-04-13 07:14:56 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+'use strict';
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+//function for text message
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+//function for score
+const displayScore = function (number) {
+  document.querySelector('.score').textContent = number;
+};
+//game logic
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  // if wrong number or no number
+  if (!guess) {
+    displayMessage('⛔️ No Number!');
+    // when player wins
+  } else if (guess === secretNumber) {
+    displayMessage('🥳 Correct Number!');
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
+    //highscore
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
+    // when guess is too high or too low
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      displayMessage(guess > secretNumber ? '📈 Too high!' : '📉 Too low!');
+      score--;
+      displayScore(score);
+    } else {
+      displayMessage('💀 You lost!');
+      displayScore(0);
+    }
+  }
+});
+// 'Again' reset btn
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('body').style.backgroundColor = '#222';
+  document.querySelector('.number').style.width = '15rem';
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  document.querySelector('.number').textContent = '?';
+  score = 20;
+  displayScore(score);
+  displayMessage('Start guessing...');
+  document.querySelector('.guess').value = '';
+});
